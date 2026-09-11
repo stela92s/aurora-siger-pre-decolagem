31e14b1ecf3d78602b1d29a77224de223174a7cc
Stela Santos <57561105+stela92s@users.noreply.github.com>
2026-09-11 22:53:26 +0100

    Update telemetria.py

--- src/telemetria.py
+++ src/telemetria.py
@@ -1,1 +1,23 @@
+import json
 
+# Abrir o arquivo de telemetria
+with open("data/telemetria_normal.json", "r") as arquivo:
+
+    # Ler os dados do arquivo
+    telemetria = json.load(arquivo)
+
+
+# Mostrar os dados na tela
+print("===================================")
+print("       AURORA SIGER")
+print("   DADOS DE TELEMETRIA")
+print("===================================")
+
+print()
+
+print("Temperatura interna:", telemetria["temperatura_interna"], "°C")
+print("Temperatura externa:", telemetria["temperatura_externa"], "°C")
+print("Integridade estrutural:", telemetria["integridade_estrutural"])
+print("Energia:", telemetria["energia"], "%")
+print("Pressão dos tanques:", telemetria["pressao_tanques"], "%")
+print("Módulos críticos:", telemetria["modulos_criticos"])
